99c7a1d3f72b7047b0d1db23cce67028f17924ac
Andrey Dolganov <dolganov2002@mail.ru>
2025-03-30 23:25:01 +0300

    add user model, views

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -4,9 +4,6 @@ from task_manager.user import views
 app_name = 'user'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('create/', views.UserCreate.as_view(), name='create'),
-    path('<int:pk>/delete/', views.UserDelete.as_view(), name='delete'),
-    path('<int:pk>/update/', views.UserUpdate.as_view(), name='update'),
-    path('<int:pk>/', views.UserDetail.as_view(), name='detail'),
+    path('', views.IndexView.as_view(), name='user_index'),
+
 ]
--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
+
 
 # Create your views here.
+class IndexView(ListView):
+    model = User
+    template_name = 'user/index.html'
+    context_object_name = 'users'
+# если не указывать, имя переменной в шаблоне (по умолчанию: `object_list` ('user_list'))
+
+class UserCreate(CreateView):
+    model = User
+    form_class = ArticleForm
+    template_name = 'articles/create.html'
+
+
+class UserUpdate(UpdateView):
+    model = User
+    form_class = ArticleForm
+    template_name = 'articles/update.html'
+
+
+class UserDelete(DeleteView):
+    model = User
+    success_url = reverse_lazy('articles:index')
+    template_name = 'articles/delete.html'
