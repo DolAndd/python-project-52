28968fe38774b724506a291d58cac503c05ba922
Andrey Dolganov <dolganov2002@mail.ru>
2025-04-07 23:11:13 +0300

    add statuses model and Index view

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -3,8 +3,6 @@ from task_manager.statuses import views
 
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='status_index'),
-    path('create/', views.StatusCreateView.as_view(), name='status_create'),
-    path('<int:pk>/update/', views.StatusUpdateView.as_view(), name='status_update'),
-    path('<int:pk>/delete/', views.StatusDeleteView.as_view(), name='status_delete'),
+    path('', views.StatusIndexView.as_view(), name='status_index'),
+
 ]
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from task_manager.statuses.models import Status
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from task_manager.user.forms import UserRegistrationForm
+from task_manager.mixins import UserPassesMixin
 
+
+# Create your views here.
+class StatusIndexView(ListView):
+    model = Status
+    template_name = 'statuses/index.html'
+    context_object_name = 'statuses'
 # Create your views here.
