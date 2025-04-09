b265f7be549b507e32df5315f872a6d719fb0dc8
Andrey Dolganov <dolganov2002@mail.ru>
2025-04-09 21:26:57 +0300

    add task model

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -1,10 +1,9 @@
 from django.forms import ModelForm
 from task_manager.statuses.models import Status
 from django import forms
-from django.utils.translation import gettext_lazy as _
+
 
 class StatusForm(ModelForm):
-    name = forms.CharField(label=_("Имя"), max_length=100, required=True)
 
     class Meta:
         model = Status
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from task_manager.statuses.models import Status
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Tasks(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name=_("Имя"))
+    description = models.TextField(blank=True, verbose_name=_("Описание"))
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, verbose_name=_("Статус"))
+    author = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name=_("Автор"))
+    executor = models.ForeignKey(User, blank=True, on_delete=models.PROTECT, verbose_name=_("Исполнитель"))
+    labels = models.ManyToManyField(Label, blank=True, verbose_name=_("Метки"))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
