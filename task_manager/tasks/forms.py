377421ba2b9598953fab1216b1a25bbca3b62d4c
DolAndd <dolganov2002@mail.ru>
2025-04-17 17:32:05 +0300

    update update task form

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -4,6 +4,13 @@ from task_manager.tasks.models import Task
 
 
 class TaskForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Переопределяем отображение исполнителя
+        self.fields["executor"].label_from_instance = (
+            lambda obj: f"{obj.first_name} {obj.last_name}".strip()
+                        or obj.username
+        )
 
     class Meta:
         model = Task
