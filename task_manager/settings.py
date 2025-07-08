32d61c30d2ae2aec190b502090fac467b38590f5
DolAndd <dolganov2002@mail.ru>
2025-07-08 11:35:56 +0300

    update build.sh with collectstatic

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
+    'django.contrib.staticfiles',
 ]
 
 ROOT_URLCONF = 'task_manager.urls'
