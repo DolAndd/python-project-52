496cb1134092e43777f1302c362d170625025c83
DolAndd <dolganov2002@mail.ru>
2025-07-19 16:07:32 +0300

    update auth yandex with custom_pipeline path

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -95,11 +95,12 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
+    'task_manager.pipeline.get_username',
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
-    'task_manager.pipeline.get_username',  # Ваш кастомный pipeline
+      # Ваш кастомный pipeline
 )
 
 SESSION_COOKIE_SECURE = True
